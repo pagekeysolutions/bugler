d6ff179df44db19a0f672fc39d7135d1851dbbfd
Stephen Grice <stephengrice@live.com>
2018-03-18 21:14:10 -0400

    Get first iteration of init-build-server working
    Though sinfully forget to test this functionality

--- lib/bugler.rb
+++ lib/bugler.rb
@@ -1,4 +1,6 @@
 require 'bugler/config'
+require 'webrick'
+require 'fileutils'
 
 module Bugler
 	def self.hi
@@ -6,12 +8,18 @@ module Bugler
 	end
 
 	module CLI
-		def self.exec(argv)
+		def self.exec(argv, working_directory=__dir__)
 			if argv.empty?
 				print Bugler::Config::HELP_TEXT
 			elsif argv[0] == 'init'
 				argv.shift
 				self.init(argv)
+			elsif argv[0] == 'server'
+				self.server working_directory
+			elsif argv[0] == 'build'
+				self.build working_directory
+			else
+				raise Bugler::Config::ERROR_EXEC_UNRECOGNIZED
 			end
 		end
 		def self.init(argv)
@@ -28,9 +36,32 @@ module Bugler
 				File.write(f, "") unless File.exists?(f)
 			end
 		end
-		def self.server
+		def self.server(working_directory, port=Bugler::Config::DEFAULT_PORT)
 			raise Bugler::Config::ERROR_SERVER_NOT_A_PROJECT unless File.exists?(".bugler")
-			
+
+			# Build if not already built
+			if !File.exists?(File.join(working_directory, "build"))
+				self.build(working_directory)
+			end
+
+			puts "Starting Bugler test server on port %i..." % port
+
+			server = WEBrick::HTTPServer.new :Port => port, :DocumentRoot => File.join(working_directory, 'build')
+
+			trap 'INT' do
+				server.shutdown
+			end
+
+			server.start
+		end
+		def self.build(working_directory)
+			raise Bugler::Config::ERROR_SERVER_NOT_A_PROJECT unless File.exists?(".bugler")
+			Dir.mkdir 'build' unless File.exists?('build')
+			# For now, copy over the erbs to htmls
+			Dir.foreach File.join(working_directory,'src') do |f|
+				next if f == '.' or f == '..'
+				FileUtils.cp File.join('src', f), File.join(working_directory, 'build', f.sub!('erb', 'html'))
+			end
 		end
 	end
 end
--- lib/bugler/config.rb
+++ lib/bugler/config.rb
@@ -7,10 +7,13 @@ module Bugler
   Usage:
     bugler init <NAME> - Generate new blank website
 }
+    ERROR_EXEC_UNRECOGNIZED = "Error: Command not recognized."
     ERROR_INIT_NO_NAME = "Error: Please provide a name for your app"
     ERROR_INIT_ARGV = "Error: Provided argv argument must be an array"
     ERROR_SERVER_NOT_A_PROJECT = "Error: Current directory is not a bugler project. Try 'bugler init <NAME>'"
 
     SOURCE_DIRNAME = "src"
+
+    DEFAULT_PORT = 8080
   end
 end
